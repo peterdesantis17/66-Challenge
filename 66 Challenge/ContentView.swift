ad1acfbee1cada1c3f10331848acb168e3dc6290
peterdesantis17 <peterdesantis2003@gmail.com>
2025-02-06 17:13:48 -0500

    Initial Commit

---
+++ 66 Challenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  66 Challenge
+//
+//  Created by Peter on 2025-02-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 66 Challenge/_6_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  _6_ChallengeApp.swift
+//  66 Challenge
+//
+//  Created by Peter on 2025-02-06.
+//
+
+import SwiftUI
+
+@main
+struct _6_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
